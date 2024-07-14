21e3a5bd2e2a99358f36cdd290a89c0329dcb68b
Nguyễn Chánh Đạt <chanhdat2504@gmail.com>
2024-07-14 19:31:18 +0700

    feat-Build Services, make DTO transform function
    build function to transform upComing, onCasting film to DTO required
    
    CU-86epuee0t

--- src/utils/mainPageFilmDTO.js
+++ src/utils/mainPageFilmDTO.js
@@ -1,26 +1,33 @@
-export function changeFilmToDTO(filmObj) {
-  const subtitleEx = filmObj.subtitle ? 'VN' : 'none subtitle';
+export function changeFilmToDTO({
+  id,
+  poster,
+  ageRate,
+  format,
+  filmName,
+  trailer,
+  category,
+  language,
+  subtitle,
+  duration,
+}) {
+  const subtitleEx = subtitle ? 'VN' : 'none subtitle';
   const filmDTO = {
-    id: filmObj.id,
-    poster: filmObj.poster,
-    ageRating: filmObj.ageRate,
-    format: filmObj.format,
-    title: filmObj.filmName,
-    trailerLink: filmObj.trailer,
-    category: filmObj.category,
-    nation: filmObj.language,
+    id,
+    poster,
+    ageRating: ageRate,
+    format: format,
+    title: filmName,
+    trailerLink: trailer,
+    category,
+    nation: language,
     subtitle: subtitleEx,
-    duration: filmObj.duration,
+    duration,
   };
 
   return filmDTO;
 }
 
 export function changeFilmListToDTO(filmObjArr) {
-  const filmListLen = filmObjArr.length;
-  const filmDTOArr = [];
-  for (let i = 0; i < filmListLen; i++) {
-    filmDTOArr.push(changeFilmToDTO(filmObjArr[i]));
-  }
+  const filmDTOArr = filmObjArr.map(changeFilmToDTO);
   return filmDTOArr;
 }
