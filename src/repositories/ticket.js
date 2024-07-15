ce28156f6c156b6340801a092e4b4e2b82c9bb6a
Nguyễn Chánh Đạt <chanhdat2504@gmail.com>
2024-07-15 11:43:01 +0700

    feat-make repo, service to getUser by username
    make repo, service to getUser by username, update model user
    
    CU-86epuh9hu

---
+++ src/repositories/ticket.js
@@ -0,0 +1,47 @@
+import { db } from '../models/index.js';
+const ticket = db.ticket;
+
+export async function getAll() {
+    return await ticket.findAll();
+}
+
+export async function getById(ticketId) {
+    return await ticket.findOne({
+        where: {
+            id: ticketId
+        }
+    });
+}
+
+export async function updateTicketPayment(ticketId, isPaid) {
+    await ticket.update(
+        {
+            isPaid: isPaid
+        },
+        {
+            where: {
+                id: ticketId
+            }
+        }
+    );
+}
+
+export async function create(ticketInfo) {
+    return await ticket.create(ticketInfo);
+}
+
+export async function drop(ticketId) {
+    await ticket.destroy({
+        where: {
+            id: ticketId
+        }
+    });
+}
+
+export async function getByUserId(userId) {
+    return await ticket.findAll({
+        where: {
+            userId: userId
+        }
+    });
+}
