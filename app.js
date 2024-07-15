90ee5d664ea22e6b943353c99086166c50da35c9
Nguyễn Chánh Đạt <chanhdat2504@gmail.com>
2024-07-15 11:43:01 +0700

    feat-make repo, service to getUser by username, pass
    make repo, service to getUser by username, pass, update model user
    
    feat-86epuh9hu

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cinemaRoute from './src/routes/cinema.js';
 import provinceRoute from './src/routes/provinceCity.js';
 import screenRoute from './src/routes/screen.js';
+import userRoute from './src/routes/user.js';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
@@ -17,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/cinema', cinemaRoute);
 app.use('/provinceCity', provinceRoute);
 app.use('/screen', screenRoute);
+app.use('/user', userRoute);
 
 app.listen(PORT, (error) => {
   if (error) console.log("Error occurred, server can't start", error);
