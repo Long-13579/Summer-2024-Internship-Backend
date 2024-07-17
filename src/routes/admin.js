57da2d669307e2e8b5e3b619c3c3e538052842e4
Nguyễn Chánh Đạt <chanhdat2504@gmail.com>
2024-07-17 10:55:45 +0700

    feat-make controller, route to add, update Film
    make controller, route to add, update Film
    
    CU-86epqjmjh

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import showAdminRoutes from './showAdmin.js';
 import screenAdminRoutes from './screenAdmin.js';
+import filmAdminRoute from './filmAdmin.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
 
 const router = express.Router();
@@ -9,5 +10,6 @@ router.use('/', verifyToken);
 
 router.use('/show', showAdminRoutes);
 router.use('/screen', screenAdminRoutes);
+router.use('/film', filmAdminRoute);
 
 export default router;
