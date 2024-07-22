34c43316f83562632158ffc9bde06a13d6e92449
Nguyễn Chánh Đạt <chanhdat2504@gmail.com>
2024-07-22 11:15:17 +0700

    feat-make Login controller, route
    build service, controller, route to get token for admin
    
    CU-86epuh5fp

---
+++ src/services/user.js
@@ -0,0 +1,22 @@
+import * as user from '../repositories/user.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { TOKEN_EXPIRE_DATE } from '../constants/token.js';
+
+export async function getToken({ userName, password }) {
+  const userNameReq = userName;
+  const passwordReq = password;
+  const dbUserInfor = await user.getByUserName(userNameReq);
+  if (!dbUserInfor.roleId || dbUserInfor === null) {
+    return;
+  }
+  const validPassword = await bcrypt.compare(passwordReq, dbUserInfor.password);
+
+  if (validPassword) {
+    const token = jwt.sign({ data: dbUserInfor }, process.env.TOKEN_SECRET, {
+      expiresIn: TOKEN_EXPIRE_DATE,
+    });
+    return token;
+  }
+  return;
+}
