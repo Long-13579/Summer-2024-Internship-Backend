cb166af3775ac53c25132fa6d190283b94b13a44
Nguyễn Chánh Đạt <chanhdat2504@gmail.com>
2024-07-22 15:17:05 +0700

    feat-update cinema model, update Repo to delete cinema
    Add status prop to cinema model, make repo to soft delete cinema
    
    CU-86epugfm0

--- src/repositories/cinema.js
+++ src/repositories/cinema.js
@@ -8,12 +8,15 @@ export async function add({ name, address, provinceCityId }) {
   });
 }
 
-export async function drop(id) {
-  await db.cinema.destroy({
-    where: {
-      id: id,
-    },
-  });
+export async function inactive(id) {
+  await db.cinema.update(
+    { status: 0 },
+    {
+      where: {
+        id: id,
+      },
+    }
+  );
 }
 export async function update({ id, name, address, provinceCityId }) {
   await db.cinema.update(
