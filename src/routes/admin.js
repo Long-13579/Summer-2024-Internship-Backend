7f80ee19e0fbca19b21569ae680193d0cea606f3
Nguyễn Chánh Đạt <chanhdat2504@gmail.com>
2024-07-25 09:38:33 +0700

    feat-make controller, route to add show
    make controller, route to add show
    
    CU-86epwrm6b

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import screenAdminRoutes from './screenAdmin.js';
+import showAdminRoutes from './showAdmin.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
 
 const router = express.Router();
@@ -7,5 +8,7 @@ const router = express.Router();
 router.use('/', verifyToken);
 
 router.use('/screen', screenAdminRoutes);
+
+router.use('/show', showAdminRoutes);
 
 export default router;
