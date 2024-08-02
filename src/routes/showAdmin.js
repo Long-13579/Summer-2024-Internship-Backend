cdc8a3f06a2529ae3cbde356a0d2791812243e04
Nguyễn Chánh Đạt <chanhdat2504@gmail.com>
2024-08-02 11:31:13 +0700

    feat-make controller, route, middleware to add show
    make controller, route to add show
    add validate dateStart, validate filmId, validate showTime to post show
    
    CU-86epwrm6b

--- src/routes/showAdmin.js
+++ src/routes/showAdmin.js
@@ -1,5 +1,7 @@
 import * as showController from '../controllers/show.js';
 import { validateShowId } from '../middlewares/show.js';
+import { validateFilmId } from '../middlewares/film.js';
+import { validateDateFilm } from '../middlewares/dateShow.js';
 import express from 'express';
 
 const router = express.Router();
@@ -7,5 +9,7 @@ const router = express.Router();
 router.delete('/:showId', validateShowId, showController.deactivateShow);
 
 router.get('/', showController.getShowForAdmin);
+
+router.post('/', validateFilmId, validateDateFilm, showController.add);
 
 export default router;
