a455db6ad6747c75dc3d66c373ca7c1c6f7b97df
Nguyễn Chánh Đạt <chanhdat2504@gmail.com>
2024-08-02 11:03:28 +0700

    Add validate filmId, validate dateStart to post show

--- src/middlewares/dateShow.js
+++ src/middlewares/dateShow.js
@@ -1,20 +1,27 @@
 import { API_STATUS } from '../models/apiStatus.js';
+import { StatusCodes } from 'http-status-codes';
 import * as showServices from '../repositories/show.js';
 
 export async function validateDateFilm(req, res, next) {
   try {
-    const dateCheck = req.body.date || req.query.date;
+    const dateCheck = req.body.dateStart || req.query.dateStart;
+    const filmIdCheck = req.body.filmId || req.query.filmId;
     if (dateCheck == undefined) {
       req.noDate = true;
       next();
       return;
     }
 
-    const showByDateInfor = await showServices.getByDate(dateCheck);
-    const noShowByDate = showByDateInfor?.length === 0;
-    if (noShowByDate) {
-      res.status(API_STATUS.NOT_FOUND.status);
-      res.send(API_STATUS.NOT_FOUND.getErrorMessage('film', 'date', dateCheck));
+    const invalidShowByDateInfor = await showServices.checkDateFilmIdToAddShow({
+      dateStart: dateCheck,
+      filmId: filmIdCheck,
+    });
+    console.log(invalidShowByDateInfor);
+    if (invalidShowByDateInfor) {
+      res.status(StatusCodes.NOT_FOUND);
+      res.send(
+        "show's dateStart is not appropriate to Film's dateStart, dateEnd"
+      );
       return;
     }
     next();
--- src/routes/showAdmin.js
+++ src/routes/showAdmin.js
@@ -1,12 +1,14 @@
 import * as showController from '../controllers/show.js';
 import { validateShowId } from '../middlewares/show.js';
+import { validateFilmId } from '../middlewares/film.js';
+import { validateDateFilm } from '../middlewares/dateShow.js';
 import express from 'express';
 
 const router = express.Router();
 
 router.get('/', showController.getShowForAdmin);
 
-router.post('/', showController.add);
+router.post('/', validateFilmId, validateDateFilm, showController.add);
 
 router.put('/', validateShowId, showController.update);
 
